8d3f6bde6a140af0e362d497406996bdb40ddfb1
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-07-13 11:40:38 +0300

    small updates

--- src/main/java/app/OneManBand.java
+++ src/main/java/app/OneManBand.java
@@ -1,15 +1,11 @@
 package app;
 
-import java.util.Map;
-
-import app.Instrument;
-import app.PerformanceException;
-import app.Performer;
+import java.util.Properties;
 
 public class OneManBand implements Performer {
-    private Map<String, Instrument> instruments;
+    private Properties instruments;
 
-    public void setInstruments(Map<String, Instrument> instruments) {
+    public void setInstruments(Properties instruments) {
         this.instruments = instruments;
     }
 
@@ -18,9 +14,9 @@ public class OneManBand implements Performer {
 
     @Override
     public void perform() throws PerformanceException {
-        for (String key: instruments.keySet()){
+        for (Object key: instruments.keySet()){
             System.out.println(key+ ": ");
-            Instrument instrument = instruments.get(key);
+            Instrument instrument = (Instrument) instruments.get(key);
             instrument.play();
         }
     }
