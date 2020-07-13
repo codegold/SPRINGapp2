c31aa7088eaa6ee2aaf54a3a37127f2a114df9a7
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-07-13 23:07:48 +0300

    Additional DI. PhoneEditor.

--- src/main/java/app/PhoneEditor.java
+++ src/main/java/app/PhoneEditor.java
@@ -3,6 +3,24 @@ package app;
 public class PhoneEditor extends java.beans.PropertyEditorSupport {
     @Override
     public void setAsText(String textValue) {
-        String stripped = str
+        String stripped = stripNonNumeric(textValue);
+
+        String areaCode = stripped.substring(0, 3);
+        String prefix = stripped.substring(3, 6);
+        String number = stripped.substring(6);
+        PhoneNumber phone = new PhoneNumber(areaCode, prefix, number);
+        setValue(phone);
+    }
+
+    private String stripNonNumeric(String original) {
+        StringBuffer allNumeric = new StringBuffer();
+
+        for (int i = 0; i < original.length(); i++) {
+            char c = original.charAt(i);
+            if (Character.isDigit(c)) {
+                allNumeric.append(c);
+            }
+        }
+        return allNumeric.toString();
     }
 }
