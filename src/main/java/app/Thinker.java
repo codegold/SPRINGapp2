81370d323ac9ce0bc144c9a73a31cfd12d2c496f
Arthur P <38310511+MaxRubshik@users.noreply.github.com>
2020-07-17 14:58:30 +0300

    thinker volunteer

---
+++ src/main/java/app/Thinker.java
@@ -0,0 +1,5 @@
+package app;
+
+public interface Thinker {
+    void thinkOfSomething(String thoughts);
+}
---
+++ src/main/java/app/Volunteer.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Volunteer implements Thinker {
+    private String thoughts;
+
+    @Override
+    public void thinkOfSomething(String thoughts) {
+        this.thoughts = thoughts;
+    }
+
+    public String getThoughts() {
+        return thoughts;
+    }
+}
